ff7f59d78525a8faf86ffd64591a4a07bcba8879
unamedkr <unamed.kr@gmail.com>
2014-04-18 04:36:03 +0900

    기본기능 완료

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/mean-dev',
+	db: 'mongodb://localhost/tpdnjfgh-dev',
 	app: {
 		title: '세월호 소통채널'
 	},
--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/mean',
+    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/tpdnjfgh',
 	facebook: {
 		clientID: '1422107211381754',
 		clientSecret: '92613031a3fe25af9388502f71bbc1d5',
